9be5c70e9f2f26bc40f851621bd607c5ee7b7b32
Takuma Mori <takuma104@gmail.com>
2026-02-14 01:31:08 +0900

    run1 setting

--- finetuning/tokenizer48k/train_upsampler.sh
+++ finetuning/tokenizer48k/train_upsampler.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 uv run accelerate launch train_upsampler.py \
-    --train_shards ".../train/*.tar" \
-    --val_shards ".../val/*.tar" \
-    --output_dir output/upsampler_run5 \
+    --train_shards "/mnt/4tb/takuma/dataset/speech/qwen3tts_tokenizer/train/*.tar" \
+    --val_shards "/mnt/4tb/takuma/dataset/speech/qwen3tts_tokenizer/val/*.tar" \
+    --output_dir output/upsampler_run1 \
     --batch_size 8 \
     --lr 1e-4 \
-    --max_train_steps 100000 \
+    --max_train_steps 1000000 \
     --l1_weight 0.0 \
     --stft_weight 1.0 \
     --mel_weight 1.0 \
     --rms_weight 10.0 \
-    --log_with wandb
+    --log_with wandb \
+    --wandb_project qwen3tts_tokenizer48k \
+    --wandb_run_name run1
